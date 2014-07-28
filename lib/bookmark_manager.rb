2ed78d343fc04572d949c66106c16991f19a5ec3
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-28 13:54:46 +0100

    Added User browsing feature in index.erb

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
+require 'data_mapper'
+require './lib/link'
+
+env = ENV["RACK_ENV"] || "development"
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello BookmarkManager!'
-  end
+
+	set :views, Proc.new { File.join(root, "..", "views") }
+
+	get '/' do
+		@links = Link.all
+		erb :index
+	end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
