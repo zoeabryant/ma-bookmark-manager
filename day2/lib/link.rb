e7a0e115d33ce7ea05222554bc07e6791df51337
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-29 10:33:13 +0100

    Splitting into Day1 and Day2

---
+++ day2/lib/link.rb
@@ -0,0 +1,10 @@
+class Link
+
+	include DataMapper::Resource
+
+	property :id,	Serial
+	property :title, String
+	property :url, String
+	has n, :tags, :through => Resource
+
+end
---
+++ day2/spec/features/user_managment_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature "User signs up" do
+
+	scenario "when being logged out" do
+		expect{ sign_up }.to change(User, :count).from(0).to(1)
+		expect(page).to have_content("Welcome, alice@example.com")
+		expect(User.first.email).to eq("alice@example.com")
+	end
+
+	scenario "with a password that doesn't match" do
+		expect{ sign_up('a@a.com', 'pass', 'wrong')}.not_to change(User, :count)
+		expect(current_path).to eq('/users')
+		expect(page).to have_content("Sorry, your passwords don't match")
+	end
+
+	def sign_up(email = "alice@example.com",
+				password = "oranges!",
+				password_confirmation = "oranges!")
+		visit '/users/new'
+		expect(page.status_code).to eq(200)
+		fill_in :email, with: email
+		fill_in :password, with: password
+		fill_in :password_confirmation, with: password_confirmation
+		click_button "Sign up"
+	end
+
+end
