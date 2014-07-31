09225027185c43d74d398b8859b59f9d10ff9b58
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-31 10:54:02 +0100

    DB based on env

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,10 +1,7 @@
 env = ENV["RACK_ENV"] || "development"
 
-if env == 'production'
-	DataMapper.setup(:default, ENV["HEROKU_POSTGRESQL_MAUVE_URL"])
-else
-	DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-end
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV["HEROKU_POSTGRESQL_MAUVE_URL"]) if env == 'production'
 
 DataMapper.finalize
 DataMapper.auto_migrate!
