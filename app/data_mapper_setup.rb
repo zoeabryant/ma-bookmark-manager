a579a800d305879287434e03616ed036df9e374a
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-31 10:48:24 +0100

    Heroku DB link

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
 
-# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://qxxtoiirwkpbrx:WYNH9sLMUNz5N7hCoACj0nImkx@ec2-107-20-191-205.compute-1.amazonaws.com:5432/d9b84p4h4p2pi1")
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}" || "postgres://qxxtoiirwkpbrx:WYNH9sLMUNz5N7hCoACj0nImkx@ec2-107-20-191-205.compute-1.amazonaws.com:5432/d9b84p4h4p2pi1")
+DataMapper.setup(:default "postgres://qxxtoiirwkpbrx:WYNH9sLMUNz5N7hCoACj0nImkx@ec2-107-20-191-205.compute-1.amazonaws.com:5432/d9b84p4h4p2pi1")
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
 DataMapper.auto_migrate!
